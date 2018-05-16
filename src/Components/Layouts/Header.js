90559058f416dac94f2493e211f0987fa63bef19
Albert Hansrisuk <alhansrisuk@gmail.com>
2018-05-16 14:23:55 -0400

    added sample golf club information to store.js

--- src/Components/Layouts/Header.js
+++ src/Components/Layouts/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { AppBar, Toolbar, Typography } from 'material-ui'
+import green from '@material-ui/core/colors/green';
 
 
 export default props =>
-    <AppBar position="static">
+    <AppBar position="static" style={{ backgroundColor: green[700] }}>
         <Toolbar>
             <Typography variant="headline" color="inherit">
                 Golf Clubs Database
---
+++ src/Components/store.js
@@ -0,0 +1,84 @@
+// Initial data set for Clubs
+export const clubtype = [
+    'driver', 'woods', 'irons', 'wedges', 'putter'
+]
+
+export const clubs = [
+    {
+        id: 'taylormade-m2-driver',
+        brand: 'taylormade',
+        model: 'm2',
+        description: 'my driver...',
+        clubtype: 'driver'
+    },
+    {
+        id: 'titleist-917',
+        brand: 'titleist',
+        model: '917',
+        description: 'a driver...',
+        clubtype: 'driver'
+    },
+    {
+        id: 'taylormade-m2-wood',
+        brand: 'taylormade',
+        model: 'm2',
+        description: 'my 3 wood...',
+        clubtype: 'woods'
+    },
+    {
+        id: 'callaway-rogue',
+        brand: 'callaway',
+        model: 'rogue',
+        description: 'a 3 wood...',
+        clubtype: 'woods'
+    },
+    {
+        id: 'taylormade-rbz2',
+        brand: 'taylormade',
+        model: 'rbz2',
+        description: 'my hybrid...',
+        clubtype: 'woods'
+    },
+    {
+        id: 'bridgestone-j40',
+        brand: 'bridgestone',
+        model: 'j40',
+        description: 'my irons...',
+        clubtype: 'irons'
+    },
+    {
+        id: 'titleist-716-ap2',
+        brand: 'titleist',
+        model: '716-ap2',
+        description: 'a set of irons...',
+        clubtype: 'irons'
+    },
+    {
+        id: 'titleist-vokey-sm5',
+        brand: 'titleist',
+        model: 'vokey-sm5',
+        description: 'my wedges...',
+        clubtype: 'wedges'
+    },
+    {
+        id: 'callaway-md3',
+        brand: 'callaway',
+        model: 'md3',
+        description: 'a wedge...',
+        clubtype: 'wedges'
+    },
+    {
+        id: 'ping-anser',
+        brand: 'ping',
+        model: 'anser',
+        description: 'my putter...',
+        clubtype: 'putter'
+    },
+    {
+        id: 'odyssey-whitehot-rx2',
+        brand: 'odyssey',
+        model: 'whitehot-rx2',
+        description: 'a putter',
+        clubtype: 'putter'
+    },
+]
